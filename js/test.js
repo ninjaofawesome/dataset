487d35094209539229a0d8256007ad751d1098ba
Hannah Nordgren <hey@hereshannahs.info>
2014-08-29 09:09:26 -0400

    added another version

--- js/test.js
+++ js/test.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
   console.log('are you ready to test some dataaaaaaaa?!');
 
-  $('.box-2').hide();
+  $('.box.box-2').hide();
+  $('.box.box-3').hide();
+  $('.box.box-4').hide();
 
   var myData = document.querySelector('.box-label');
-  console.log(myData.dataset.color);
+  var moreData = $('.box-label-2');
+  var answer= $('.box-label-2').data('color');
 
   $('.box').click(function(){
-    $('.box-2').children('h1').text(myData.dataset.color);
-    $('.box-2').fadeIn();
+    $('.box.box-2').children('h1').text(myData.dataset.color);
+    $('.box.box-2').fadeIn();
+    $('.box.box-3').delay(600).fadeIn();
+  });
 
+  $('.box.box-3').click(function(){
+    $('.box.box-4').children('h1').text(answer);
+    $('.box.box-4').fadeIn();
   });
 });
