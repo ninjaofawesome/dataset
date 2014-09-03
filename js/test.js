c7909493efb34cb4e63868ee2af7c0b38b06eafb
Hannah Nordgren <hey@hereshannahs.info>
2014-09-03 00:01:24 -0400

    added attribute

--- js/test.js
+++ js/test.js
@@ -6,17 +6,18 @@ $(document).ready(function(){
   $('.box.box-4').hide();
 
   var myData = document.querySelector('.box-label');
-  var moreData = $('.box-label-2');
   var answer= $('.box-label-2').data('color');
 
   $('.box').click(function(){
-    $('.box.box-2').children('h1').text(myData.dataset.color);
+    $('.box.box-2').children('h1').text(myData.dataset.color)
+
     $('.box.box-2').fadeIn();
     $('.box.box-3').delay(600).fadeIn();
   });
 
   $('.box.box-3').click(function(){
-    $('.box.box-4').children('h1').text(answer);
+    $('.box.box-4').children('h1').text(answer).attr('data-number', '1');
     $('.box.box-4').fadeIn();
   });
 });
+
